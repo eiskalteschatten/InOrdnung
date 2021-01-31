291eb4a334a4e68a94af8bf19f15c451ae7bf121
Alex <alex@alexseifert.com>
2021-01-31 17:57:44 +0100

    Remove accidentally committed changes

--- src/store/actions/appActions.ts
+++ src/store/actions/appActions.ts
@@ -1,19 +1,13 @@
 import { Action } from 'redux';
 
 import {
-  APP_PLATFORM,
-  APP_PROCESS_VERSIONS
+  APP_PLATFORM
 } from '../constants';
 
 export interface AppSetPlatform extends Action<typeof APP_PLATFORM> {
   platform: string;
 }
 
-export interface AppSetProcessVersions extends Action<typeof APP_PROCESS_VERSIONS> {
-  versions: string;
-}
-
 export type AppActions = AppSetPlatform;
 
 export const appSetPlatform = (platform: string): AppSetPlatform => ({ type: APP_PLATFORM, platform });
-export const appSetProcessVersions = (versions: string): AppSetProcessVersions => ({ type: APP_PROCESS_VERSIONS, versions });
