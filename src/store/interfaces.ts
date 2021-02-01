0aa42dc5f60a9a93dc6fda5856cddc0daa0da71b
Alex Seifert <alex@alexseifert.com>
2021-02-01 13:35:12 +0100

    Add comment with link

--- src/store/interfaces.ts
+++ src/store/interfaces.ts
@@ -3,4 +3,5 @@ import { ThunkAction } from 'redux-thunk';
 
 import { State } from '.';
 
+// See https://redux.js.org/recipes/usage-with-typescript#usage-with-redux-thunk
 export type ReduxThunk<T, A> = ThunkAction<T, State, unknown, Action<A>>;
