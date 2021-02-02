de3ef177c5498b88fcb56dceecff22733e11e60f
Alex <alex@alexseifert.com>
2021-02-02 23:24:40 +0100

    Hook up the project image to Redux

--- src/components/ProjectInfo/ProjectImage/index.tsx
+++ src/components/ProjectInfo/ProjectImage/index.tsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
 import clsx from 'clsx';
+import { useSelector } from 'react-redux';
+
+import { State } from '../../../store';
 
 import styles from './ProjectImage.module.scss';
 
 const { ipcRenderer } = window.require('electron');
 
 const ProjectImage: React.FC = () => {
+  const projectInfo = useSelector((state: State) => state.project.data.projectInfo);
   const [isDraggingOver, setIsDraggingOver] = useState<boolean>(false);
 
   const handleOnDragOver = (e: React.DragEvent<HTMLDivElement>): void => {
@@ -42,7 +46,11 @@ const ProjectImage: React.FC = () => {
       onDrop={handleOnDrop}
       onClick={handleClick}
     >
-      <FormattedMessage id='dragOrClickProjectImage' />
+      {projectInfo?.image ? (
+        <img src={projectInfo.image} className={styles.image} />
+      ) :(
+        <FormattedMessage id='dragOrClickProjectImage' />
+      )}
     </div>
   );
 };
