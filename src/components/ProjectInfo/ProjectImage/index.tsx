0ca29f866cb376656f0ce3b333417f5ca61d22ed
Alex Seifert <alex@alexseifert.com>
2021-02-02 19:21:02 +0100

    Start dragging events for the project

--- src/components/ProjectInfo/ProjectImage/index.tsx
+++ src/components/ProjectInfo/ProjectImage/index.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FormattedMessage } from 'react-intl';
+import clsx from 'clsx';
 
 import styles from './ProjectImage.module.scss';
 
 const ProjectImage: React.FC = () => {
+  const [isDraggingOver, setIsDraggingOver] = useState<boolean>(false);
+
+  const handleOnDragOver = (e: React.DragEvent): void => {
+    e.preventDefault();
+    setIsDraggingOver(true);
+  };
+
+  const handleOnDragLeave = (e: React.DragEvent): void => {
+    e.preventDefault();
+    setIsDraggingOver(false);
+  };
+
+  const handleOnDrop = (e: React.DragEvent): void => {
+    e.preventDefault();
+    setIsDraggingOver(false);
+  };
+
+  // TODO: add context menu!
+
   return (
-    <div className={styles.projectImage}>
+    <div
+      className={clsx({
+        [styles.projectImage]: true,
+        [styles.isDragging]: isDraggingOver
+      })}
+      onDragOver={handleOnDragOver}
+      onDragLeave={handleOnDragLeave}
+      onDrop={handleOnDrop}
+    >
       <FormattedMessage id='dragOrClickProjectImage' />
     </div>
   );
