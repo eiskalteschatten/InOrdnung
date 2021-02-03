fb136ce33dc9d11a41b0f81b1b6c48daa6eb219e
Alex Seifert <alex@alexseifert.com>
2021-02-03 13:06:23 +0100

    Refactor

--- src/main/lib/project.ts
+++ src/main/lib/project.ts
@@ -3,12 +3,15 @@ import { encodeImage, getFileType, selectImage } from './images';
 
 
 export const selectProjectImage = async (sender: WebContents): Promise<void> => {
-  const result = await selectImage();
+  const { filePaths, canceled } = await selectImage();
 
-  const imagePath = result.filePaths[0];
-  if (imagePath) {
-    const image = await encodeImage(imagePath);
-    const mimeType = await getFileType(imagePath);
-    sender.send('updateProjectInfo', { image: { image, mimeType } });
+  if (!canceled) {
+    const imagePath = filePaths[0];
+
+    if (imagePath) {
+      const image = await encodeImage(imagePath);
+      const mimeType = await getFileType(imagePath);
+      sender.send('updateProjectInfo', { image: { image, mimeType } });
+    }
   }
 };
