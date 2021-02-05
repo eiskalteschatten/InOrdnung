634902e769db187cc437b46664c0c0b17381db3b
Alex <alex@alexseifert.com>
2021-02-05 23:15:10 +0100

    Add className prop to the rounded buttons

--- src/components/elements/RoundedButton/index.tsx
+++ src/components/elements/RoundedButton/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import clsx from 'clsx';
 
 import styles from './RoundedButton.module.scss';
 
 interface Props {
   onClick: () => void;
   children: any;
+  className?: string;
 }
 
-const RoundedButton: React.FC<Props> = ({ onClick, children }) => {
+const RoundedButton: React.FC<Props> = ({ onClick, children, className }) => {
   return (
-    <button className={styles.roundedButton} onClick={onClick}>
+    <button
+      className={clsx(styles.roundedButton, className)}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
