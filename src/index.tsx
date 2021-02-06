f61b14880de7732318f7b80c09643309fd4f6e42
Alex <alex@alexseifert.com>
2021-02-06 14:21:52 +0100

    Remove Bootstrap Icons CSS

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
-import 'bootstrap-icons/font/bootstrap-icons.css';
-
 import App from './App';
 import store from './store';
 import * as serviceWorker from './serviceWorker';
