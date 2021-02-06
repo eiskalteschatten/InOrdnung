c73436c352b79b8add524fd911825f2700ee105f
Alex <alex@alexseifert.com>
2021-02-06 17:22:23 +0100

    Use the Material UI datepicker

--- src/components/ProjectInfo/index.tsx
+++ src/components/ProjectInfo/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import clsx from 'clsx';
 import { useSelector, useDispatch } from 'react-redux';
+import MomentUtils from '@date-io/moment';
+import { MaterialUiPickersDate } from '@material-ui/pickers/typings/date';
+import moment from 'moment';
 
 import {
   FormControlLabel,
@@ -8,9 +11,15 @@ import {
   TextField,
 } from '@material-ui/core';
 
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+
 import { State } from '../../store';
 import { projectSetProjectInfo } from '../../store/actions/projectActions';
 import useTranslation from '../../intl/useTranslation';
+import { getDateLocaleFormat } from '../../lib/dates';
 import ProjectImage from './ProjectImage';
 
 import styles from './ProjectInfo.module.scss';
@@ -33,100 +42,111 @@ const ProjectInfo: React.FC = () => {
     }));
   };
 
+  const handleDateChange = (id: string, date: MaterialUiPickersDate): void => {
+    const saveDate = moment(date).format('yyyy-MM-DD');
+
+    dispatch(projectSetProjectInfo({
+      ...projectInfo,
+      [id]: saveDate?.toString(),
+    }));
+  };
+
   return (
-    <div className={styles.projectInfo}>
-      <ProjectImage />
+    <MuiPickersUtilsProvider utils={MomentUtils}>
+      <div className={styles.projectInfo}>
+        <ProjectImage />
 
-      <div className={styles.formGroup}>
-        <div className={styles.formControlWrapper}>
-          <TextField
-            id='name'
-            label={useTranslation('projectName')}
-            variant='outlined'
-            value={projectInfo?.name}
-            onChange={handleFieldChange}
-            className={styles.textField}
-            InputLabelProps={{ shrink: !!projectInfo?.name }}
-          />
-        </div>
-
-        <div className={styles.formControlWrapper}>
-          <TextField
-            id='description'
-            label={useTranslation('projectDescription')}
-            variant='outlined'
-            value={projectInfo?.description}
-            onChange={handleFieldChange}
-            className={styles.textField}
-            InputLabelProps={{ shrink: !!projectInfo?.name }}
-            multiline
-            rows={4}
-          />
-        </div>
-      </div>
-
-      <div
-        className={clsx(
-          'form-group',
-          styles.formGroup
-        )}
-      >
-        <div className={styles.formControlWrapper}>
-          <FormControlLabel
-            control={
-              <Switch
-                checked={projectInfo?.hasStartDate}
-                onChange={handleSwitchChange}
-                className={styles.switch}
-                name='hasStartDate'
-              />
-            }
-            label={useTranslation('projectHasStartDate')}
-          />
-
-          {projectInfo?.hasStartDate && (
-            <input
-              type='date'
-              className='form-control'
-              id='startDate'
-              value={projectInfo?.startDate}
+        <div className={styles.formGroup}>
+          <div className={styles.formControlWrapper}>
+            <TextField
+              id='name'
+              label={useTranslation('projectName')}
+              variant='outlined'
+              value={projectInfo?.name}
               onChange={handleFieldChange}
+              className={styles.textField}
+              InputLabelProps={{ shrink: !!projectInfo?.name }}
             />
-          )}
-        </div>
-      </div>
+          </div>
 
-      <div
-        className={clsx(
-          'form-group',
-          styles.formGroup
-        )}
-      >
-        <div className={styles.formControlWrapper}>
-          <FormControlLabel
-            control={
-              <Switch
-                checked={projectInfo?.hasEndDate}
-                onChange={handleSwitchChange}
-                className={styles.switch}
-                name='hasEndDate'
-              />
-            }
-            label={useTranslation('projectHasEndDate')}
-          />
-
-          {projectInfo?.hasEndDate && (
-            <input
-              type='date'
-              className='form-control'
-              id='endDate'
-              value={projectInfo?.endDate}
+          <div className={styles.formControlWrapper}>
+            <TextField
+              id='description'
+              label={useTranslation('projectDescription')}
+              variant='outlined'
+              value={projectInfo?.description}
               onChange={handleFieldChange}
+              className={styles.textField}
+              InputLabelProps={{ shrink: !!projectInfo?.name }}
+              multiline
+              rows={4}
             />
+          </div>
+        </div>
+
+        <div
+          className={clsx(
+            'form-group',
+            styles.formGroup
           )}
+        >
+          <div className={styles.formControlWrapper}>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={projectInfo?.hasStartDate}
+                  onChange={handleSwitchChange}
+                  className={styles.switch}
+                  id='hasStartDate'
+                />
+              }
+              label={useTranslation('projectHasStartDate')}
+            />
+
+            {projectInfo?.hasStartDate && (
+              <KeyboardDatePicker
+                margin='normal'
+                id='startDate'
+                format={getDateLocaleFormat()}
+                value={projectInfo?.startDate}
+                onChange={(date: MaterialUiPickersDate) => handleDateChange('startDate', date)}
+              />
+            )}
+          </div>
+        </div>
+
+        <div
+          className={clsx(
+            'form-group',
+            styles.formGroup
+          )}
+        >
+          <div className={styles.formControlWrapper}>
+            <FormControlLabel
+              control={
+                <Switch
+                  checked={projectInfo?.hasEndDate}
+                  onChange={handleSwitchChange}
+                  className={styles.switch}
+                  id='hasEndDate'
+                />
+              }
+              label={useTranslation('projectHasEndDate')}
+            />
+
+            {projectInfo?.hasEndDate && (
+              <KeyboardDatePicker
+                margin='normal'
+                id='endDate'
+                format={getDateLocaleFormat()}
+                value={projectInfo?.endDate}
+                onChange={(date: MaterialUiPickersDate) => handleDateChange('endDate', date)}
+              />
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    </MuiPickersUtilsProvider>
   );
 };
 
