0840a868ae817db5ae85bbc73d86be4b5e2d0442
Alex <alex@alexseifert.com>
2021-02-07 23:11:34 +0100

    Create the bookmarks table with mock data

--- src/components/Bookmarks/index.tsx
+++ src/components/Bookmarks/index.tsx
@@ -2,26 +2,67 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
 import {
-  Grid,
+  Table,
+  TableBody,
+  TableCell,
+  TableSortLabel,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  IconButton,
 } from '@material-ui/core';
 
+import OpenInBrowser from '@material-ui/icons/OpenInBrowser';
 
 import styles from './Bookmarks.module.scss';
 
+function createData(name: string, url: string) {
+  return { name, url };
+}
+
+const rows = [
+  createData('AlexSeifert.com', 'https://www.alexseifert.com'),
+  createData('History Rhymes', 'https://www.historyrhymes.info'),
+  createData('Developer\'s Notebook', 'https://www.developers-notebook.com'),
+];
 
 const Bookmarks: React.FC = () => {
   return (
-    <Grid container className={styles.about} spacing={5}>
-      <Grid item xs={6}>
-        Bookmark Name
-      </Grid>
-      <Grid item xs={5}>
-        URL
-      </Grid>
-      <Grid item xs={5}>
-        -&gtr;
-      </Grid>
-    </Grid>
+    <TableContainer component={Paper} className={styles.tableContainer}>
+      <Table size='small'>
+        <TableHead>
+          <TableRow>
+            <TableCell>
+              <TableSortLabel>
+                Name
+              </TableSortLabel>
+            </TableCell>
+            <TableCell>
+              <TableSortLabel>
+                URL
+              </TableSortLabel>
+            </TableCell>
+            <TableCell align='right'></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row: any) => (
+            <TableRow key={row.name}>
+              <TableCell component='th' scope='row'  className={styles.tableCell}>
+                {row.name}
+              </TableCell>
+              <TableCell className={styles.tableCell}>{row.url}</TableCell>
+              <TableCell align='right'>
+                <IconButton size='small'>
+                  <OpenInBrowser fontSize='small' />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
