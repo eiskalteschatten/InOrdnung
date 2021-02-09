bdbd06b176876cb9c6352595a518ffe390f714ff
Alex <alex@alexseifert.com>
2021-02-09 23:32:39 +0100

    Add Redux actions for tasks

--- src/store/actions/projectActions/index.ts
+++ src/store/actions/projectActions/index.ts
@@ -2,26 +2,44 @@ import { Action } from 'redux';
 
 import { Project } from '../../../interfaces/project';
 import { ProjectInfo } from '../../../interfaces/projectInfo';
+import { Task } from '../../../interfaces/tasks';
 import { Bookmark } from '../../../interfaces/bookmarks';
 
 import {
   PROJECT_SET_PROJECT,
   PROJECT_SET_PROJECT_INFO,
   PROJECT_DELETE_IMAGE,
+  PROJECT_SET_TASKS,
+  PROJECT_ADD_TASK,
   PROJECT_SET_BOOKMARKS,
   PROJECT_ADD_BOOKMARK,
 } from '../../constants';
 
+// Project
 export interface ProjectSetProject extends Action<typeof PROJECT_SET_PROJECT> {
   payload: Project;
 }
 
+
+// Project Info
 export interface ProjectSetProjectInfo extends Action<typeof PROJECT_SET_PROJECT_INFO> {
   payload: ProjectInfo;
 }
 
 export type ProjectDeleteImage = Action<typeof PROJECT_DELETE_IMAGE>;
 
+
+// Tasks
+export interface ProjectSetTasks extends Action<typeof PROJECT_SET_TASKS> {
+  payload: Bookmark[];
+}
+
+export interface ProjectAddTask extends Action<typeof PROJECT_ADD_TASK> {
+  payload: Task;
+}
+
+
+// Bookmarks
 export interface ProjectSetBookmarks extends Action<typeof PROJECT_SET_BOOKMARKS> {
   payload: Bookmark[];
 }
@@ -34,6 +52,8 @@ export type ProjectActions =
   ProjectSetProject |
   ProjectSetProjectInfo |
   ProjectDeleteImage |
+  ProjectSetTasks |
+  ProjectAddTask |
   ProjectSetBookmarks |
   ProjectAddBookmark;
 
