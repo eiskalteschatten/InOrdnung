2617a257c3c6b7d5d74a008a115b2f2801d00bc8
Alex Seifert <alex@alexseifert.com>
2021-02-10 16:55:54 +0100

    Add variants to the rounded button

--- src/components/RoundedButton/index.tsx
+++ src/components/RoundedButton/index.tsx
@@ -1,18 +1,38 @@
 import React from 'react';
 import clsx from 'clsx';
 
+import { makeStyles, Theme } from '@material-ui/core/styles';
+
 import styles from './RoundedButton.module.scss';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  contained: {
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.background.default,
+    '&:active': {
+      opacity: .9,
+    },
+  },
+}));
 
 interface Props {
   onClick: () => void;
   children: any;
   className?: string;
+  variant?: 'standard' | 'contained';
 }
 
-const RoundedButton: React.FC<Props> = ({ onClick, children, className }) => {
+const RoundedButton: React.FC<Props> = ({ onClick, children, className, variant }) => {
+  const classes = useStyles();
+
   return (
     <button
-      className={clsx(styles.roundedButton, className)}
+      className={clsx({
+        [className || '']: !!className,
+        [styles.base]: true,
+        [styles.standard]: !variant || variant === 'standard',
+        [classes.contained]: variant === 'contained',
+      })}
       onClick={onClick}
     >
       <div className={styles.wrapper}>
