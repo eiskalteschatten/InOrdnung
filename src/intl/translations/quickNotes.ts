43f8212b5cd529a9dc1e69bf6d35d22c958b996e
Alex <alex@alexseifert.com>
2021-02-13 13:28:28 +0100

    Add text field for new notes

--- src/intl/translations/quickNotes.ts
+++ src/intl/translations/quickNotes.ts
@@ -1,8 +1,10 @@
 export default {
   en: {
     quickNotes: 'Quick Notes',
+    quickNotesNewNote: 'New Note',
   },
   de: {
     quickNotes: 'Schnelle Notizen',
+    quickNotesNewNote: 'Neue Notiz',
   },
 };
--- src/windows/Project/QuickNotes/index.tsx
+++ src/windows/Project/QuickNotes/index.tsx
@@ -4,18 +4,36 @@ import { useDispatch, useSelector } from 'react-redux';
 import clsx from 'clsx';
 import { IpcRendererEvent } from 'electron';
 
+import {
+  TextField,
+} from '@material-ui/core';
+
 import Add from '@material-ui/icons/Add';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+import useTranslation from '../../../intl/useTranslation';
 
 import styles from './QuickNotes.module.scss';
 
 const { ipcRenderer } = window.require('electron');
 
 const QuickNotes: React.FC = () => {
+  const handleNewNoteClick = (e: React.MouseEvent<HTMLInputElement>): void => {
+    e.preventDefault();
+    console.log('onclick');
+  };
 
   return (
     <div>
-      Quick notes
+      <div className={styles.newQuickNoteWrapper}>
+        <TextField
+          label={`${useTranslation('quickNotesNewNote')}...`}
+          variant='filled'
+          className={styles.newQuickNote}
+          onClick={handleNewNoteClick}
+          size='small'
+        />
+      </div>
     </div>
   );
 };
