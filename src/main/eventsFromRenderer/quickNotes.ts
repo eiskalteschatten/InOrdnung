9029cfd7344db8a5b790143e3b5af055ea835aaf
Alex <alex@alexseifert.com>
2021-02-13 16:23:51 +0100

    Deleting a Quick Note from the toolbar now works

--- src/main/eventsFromRenderer/quickNotes.ts
+++ src/main/eventsFromRenderer/quickNotes.ts
@@ -1,7 +1,8 @@
-import { ipcMain, IpcMainEvent, BrowserWindow, Menu } from 'electron';
+import { ipcMain, IpcMainEvent, BrowserWindow, Menu, dialog } from 'electron';
 
 import quickNoteMenuCm from '../cmMenus/quickNoteMenu';
 import { QuickNote } from '../../interfaces/quickNotes';
+import { getTranslation } from '../../lib/helper';
 
 ipcMain.on('showQuickNoteMenu', (e: IpcMainEvent, quickNote: QuickNote): void => {
   const window = BrowserWindow.fromWebContents(e.sender);
@@ -9,5 +10,22 @@ ipcMain.on('showQuickNoteMenu', (e: IpcMainEvent, quickNote: QuickNote): void =>
   if (window) {
     const menu = Menu.buildFromTemplate(quickNoteMenuCm(quickNote));
     menu.popup({ window });
+  }
+});
+
+ipcMain.on('deleteQuickNote', async (e: IpcMainEvent, quickNote: QuickNote): Promise<void> => {
+  const translation = getTranslation();
+
+  const result = await dialog.showMessageBox({
+    message: translation.quickNoteDeleteNoteConfirmation,
+    detail: translation.youCantUndoThisAction,
+    buttons: [translation.no, translation.yes],
+    type: 'warning',
+    defaultId: 0,
+    cancelId: 0,
+  });
+
+  if (result.response === 1) {
+    e.sender.send('deleteQuickNote', quickNote);
   }
 });
--- src/windows/Project/QuickNotes/NoteToolbar/index.tsx
+++ src/windows/Project/QuickNotes/NoteToolbar/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 
 import {
   IconButton,
@@ -11,12 +10,16 @@ import { QuickNote } from '../../../../interfaces/quickNotes';
 
 import styles from './NoteToolbar.module.scss';
 
+const { ipcRenderer } = window.require('electron');
+
 interface Props {
   quickNote?: QuickNote;
 }
 
 const NoteToolbar: React.FC<Props> = ({ quickNote }) => {
-  const dispatch = useDispatch();
+  const handleDeleteNote = (): void => {
+    ipcRenderer.send('deleteQuickNote', quickNote);
+  };
 
   return (
     <div className={styles.noteToolbar}>
@@ -24,7 +27,7 @@ const NoteToolbar: React.FC<Props> = ({ quickNote }) => {
 
       </div>
       <div className={styles.rightSide}>
-        <IconButton size='small' color='inherit'>
+        <IconButton size='small' color='inherit' onClick={handleDeleteNote}>
           <DeleteIcon fontSize='small' />
         </IconButton>
       </div>
