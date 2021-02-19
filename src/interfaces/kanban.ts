4930ea5e98471f84fdb8263d0d126cd8c4c0bd36
Alex <alex@alexseifert.com>
2021-02-19 22:08:48 +0100

    Finish the interface for Kanban tasks

--- src/interfaces/kanban.ts
+++ src/interfaces/kanban.ts
@@ -3,5 +3,9 @@ export interface KanbanTask {
   taskNumber?: string;
   title?: string;
   description?: string;
-
+  boardId?: string;
+  column?: number;
+  finished?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
 }
