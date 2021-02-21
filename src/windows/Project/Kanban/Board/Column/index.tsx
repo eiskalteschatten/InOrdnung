ae11f2fd91326d19152f23852900c7517a0b305b
Alex <alex@alexseifert.com>
2021-02-21 12:21:49 +0100

    Add tasks to each column

--- src/windows/Project/Kanban/Board/Column/index.tsx
+++ src/windows/Project/Kanban/Board/Column/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 import {
   Paper,
 } from '@material-ui/core';
 
-import { KanbanBoardColumn } from '../../../../../interfaces/kanban';
+import { State } from '../../../../../store';
+import { KanbanBoardColumn, KanbanTask } from '../../../../../interfaces/kanban';
 
 import styles from './Column.module.scss';
 
@@ -13,13 +15,24 @@ interface Props {
 }
 
 const Column: React.FC<Props> = ({ column }) => {
+  const allTasks = useSelector((state: State) => state.project?.kanban?.tasks);
+  const [tasks, setTasks] = useState<KanbanTask[]>([]);
+
+  useEffect(() => {
+    const columnTasks = allTasks.filter(task => task.column === column.id);
+    setTasks(columnTasks);
+  }, [allTasks]);
 
   return (
     <Paper elevation={0} className={styles.column}>
       <div className={styles.title}>{column.name}</div>
 
       <div className={styles.tasks}>
-
+        {tasks?.map(task => (
+          <div key={task.id}>
+            {task.title}
+          </div>
+        ))}
       </div>
     </Paper>
   );
