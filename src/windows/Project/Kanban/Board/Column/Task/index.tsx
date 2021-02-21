4f8fcef4875c2aad13d74c11d113f0c1b9c989c0
Alex <alex@alexseifert.com>
2021-02-21 23:42:12 +0100

    Make the tasks draggable

--- src/windows/Project/Kanban/Board/Column/Task/index.tsx
+++ src/windows/Project/Kanban/Board/Column/Task/index.tsx
@@ -1,6 +1,8 @@
-import React, { useContext } from 'react';
+import React, { useCallback, useContext, useState, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import ReactMarkdown from 'react-markdown/with-html';
+import clsx from 'clsx';
+import { debounce } from 'lodash';
 
 import {
   Paper,
@@ -22,6 +24,10 @@ interface Props {
 const Task: React.FC<Props> = ({ task, columnId }) => {
   const dispatch = useDispatch();
   const context = useContext(Context);
+  const taskRef = useRef<HTMLDivElement>(null);
+  const [isBeingDragged, setIsBeingDragged] = useState<boolean>(false);
+  const [coordinates, setCoordinates] = useState<{x?: number, y?: number}>({});
+  const [taskRect, setTaskRect] = useState<DOMRect>();
 
   const handleOpenTask = (): void => {
     context.setIsNewTask(false);
@@ -30,11 +36,58 @@ const Task: React.FC<Props> = ({ task, columnId }) => {
     dispatch(uiOpenEditKanbanTaskDialog(true));
   };
 
+  const handleMouseDown = useCallback((e: React.MouseEvent<HTMLDivElement>) => {
+    const startRect = taskRef.current?.getBoundingClientRect();
+    const diffX = e.pageX - (startRect?.left ?? 0);
+    const diffY = e.pageY - (startRect?.top ?? 0);
+
+    const handleMouseMove = (e: any): void => {
+      const rect = taskRef.current?.getBoundingClientRect();
+
+      if (rect) {
+        const x = e.pageX - diffX;
+        const y = e.pageY - diffY;
+        setCoordinates({ x, y });
+      }
+    };
+
+    const handleMouseUp = (e: any): void => {
+      e.preventDefault();
+
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mouseup', handleMouseUp);
+      // const rect = sidebarRef.current?.getBoundingClientRect();
+      // dispatch(uiSetSidebarWidth(rect?.width));
+      setIsBeingDragged(false);
+      setCoordinates({});
+    };
+
+    e.preventDefault();
+    e.stopPropagation();
+
+    setIsBeingDragged(true);
+    setTaskRect(startRect);
+
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
+  }, []);
+
   return (
     <Paper
-      className={styles.task}
+      className={clsx({
+        [styles.task]: true,
+        [styles.dragging]: isBeingDragged,
+      })}
       onClick={handleOpenTask}
       onContextMenu={() => ipcRenderer.send('showKanbanMenu', task)}
+      onMouseDown={handleMouseDown}
+      style={{
+        top: coordinates.y,
+        left: coordinates.x,
+        height: taskRect?.height,
+        width: taskRect?.width,
+      }}
+      ref={taskRef}
     >
       <div className={styles.title}>{task.title}</div>
       <div className={styles.description}>
