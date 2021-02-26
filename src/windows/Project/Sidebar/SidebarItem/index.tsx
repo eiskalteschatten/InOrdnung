79310f705e1ec398fb23893b5740e24f2d60d6cc
Alex <alex@alexseifert.com>
2021-02-26 13:22:45 +0100

    Use the theme's primary color for the selection color

--- src/windows/Project/Sidebar/SidebarItem/index.tsx
+++ src/windows/Project/Sidebar/SidebarItem/index.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import clsx from 'clsx';
 
+import { makeStyles, Theme } from '@material-ui/core/styles';
+
 import styles from './SidebarItem.module.scss';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  selected: {
+    color: theme.palette.primary.main,
+    borderLeft: `5px solid ${theme.palette.primary.main}`,
+  },
+}));
 
 export interface Props {
   path: string;
@@ -12,6 +21,7 @@ export interface Props {
 
 const SidebarItem: React.FC<Props> = ({ ItemIcon, title, path }) => {
   const location = useLocation();
+  const classes = useStyles();
 
   if (path.charAt(path.length -1) === '/') {
     path = path.slice(0, -1);
@@ -22,6 +32,7 @@ const SidebarItem: React.FC<Props> = ({ ItemIcon, title, path }) => {
       to={path}
       className={clsx({
         [styles.sidebarItem]: true,
+        [classes.selected]: location.pathname === path,
         [styles.selected]: location.pathname === path,
       })}
     >
