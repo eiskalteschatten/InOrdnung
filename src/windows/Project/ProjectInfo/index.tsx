7a2d9c28dd85921977d736aca976cdde70fd6071
Alex <alex@alexseifert.com>
2021-03-06 11:26:45 +0100

    Redesign the project image, name and description fields

--- src/windows/Project/ProjectInfo/index.tsx
+++ src/windows/Project/ProjectInfo/index.tsx
@@ -35,42 +35,38 @@ const ProjectInfo: React.FC = () => {
 
   return (
     <Container maxWidth='md'>
-      <Grid container justify='center' className={styles.projectInfo}>
-        <Grid item>
-          <ProjectImage />
-        </Grid>
-        <Grid item>
-          <div className={styles.formGroup}>
-            <div className={styles.formControlWrapper}>
-              <TextField
-                id='name'
-                label={messages.projectName}
-                variant='outlined'
-                value={projectInfo?.name ?? ''}
-                onChange={handleFieldChange}
-                className={styles.textField}
-                InputLabelProps={{ shrink: !!projectInfo?.name }}
-                fullWidth
-              />
-            </div>
+      <div className={styles.projectInfo}>
+        <ProjectImage />
 
-            <div className={styles.formControlWrapper}>
-              <TextField
-                id='description'
-                label={messages.projectDescription}
-                variant='outlined'
-                value={projectInfo?.description ?? ''}
-                onChange={handleFieldChange}
-                className={styles.textField}
-                InputLabelProps={{ shrink: !!projectInfo?.description }}
-                multiline
-                fullWidth
-                rows={7}
-              />
-            </div>
-          </div>
-        </Grid>
-      </Grid>
+        <TextField
+          id='name'
+          label={messages.projectName}
+          variant='outlined'
+          value={projectInfo?.name ?? ''}
+          onChange={handleFieldChange}
+          InputLabelProps={{ shrink: !!projectInfo?.name }}
+          className={styles.projectName}
+          fullWidth
+        />
+      </div>
+
+      <Card className={styles.card}>
+        <CardContent>
+          <CardTitle>
+            <FormattedMessage id='projectDescription' />
+          </CardTitle>
+
+          <TextField
+            id='description'
+            variant='outlined'
+            value={projectInfo?.description ?? ''}
+            onChange={handleFieldChange}
+            multiline
+            fullWidth
+            rows={4}
+          />
+        </CardContent>
+      </Card>
 
       <Card className={styles.card}>
         <CardContent>
