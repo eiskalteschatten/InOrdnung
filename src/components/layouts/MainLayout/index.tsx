4bba88fbfbf7a79185767dba6939869dc7fe3cb4
Alex Seifert <alex@alexseifert.com>
2022-10-07 15:32:08 +0200

    Start the toolbar

--- src/components/layouts/MainLayout/index.tsx
+++ src/components/layouts/MainLayout/index.tsx
@@ -4,21 +4,35 @@ import clsx from 'clsx';
 
 import { State } from '../../../store';
 import Titlebar from './components/Titlebar';
+import Toolbar from './components/Toolbar';
 
 import styles from './styles.module.scss';
 
-const MainLayout: React.FC = () => {
+interface Props {
+  toolbar?: React.ReactNode;
+  children: React.ReactNode;
+}
+
+const MainLayout: React.FC<Props> = ({ toolbar, children }) => {
   const platform = useSelector((state: State) => state.app.platform);
 
   return (
     <div
-      className={clsx({
-        [styles.isDarwin]: platform === 'darwin',
+      className={clsx(styles.mainLayout, {
+        [styles.customTitlebar]: platform === 'darwin',
       })}
     >
       {platform === 'darwin' && (<Titlebar />)}
 
-      content here
+      {toolbar && (
+        <Toolbar>
+          {toolbar}
+        </Toolbar>
+      )}
+
+      <div className={styles.content}>
+        {children}
+      </div>
     </div>
   );
 };
