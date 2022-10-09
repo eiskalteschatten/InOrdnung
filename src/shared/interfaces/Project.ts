719b7c50cd205d296119b1bd5b1a3a0a7b74a001
Alex Seifert <alex@alexseifert.com>
2022-10-09 16:39:10 +0200

    Add interfaces

--- src/shared/interfaces/Project.ts
+++ src/shared/interfaces/Project.ts
@@ -1,4 +1,10 @@
-export interface Project {
+import { Bookmark } from './Bookmark';
+
+export interface ProjectInfo {
   name: string;
   description?: string;
+}
+
+export interface Project extends ProjectInfo {
+  bookmarks?: Bookmark[];
 }
