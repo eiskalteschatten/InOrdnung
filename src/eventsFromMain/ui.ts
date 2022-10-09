f253114fc66e0ebc9b6152e2121831693c7cab57
Alex Seifert <alex@alexseifert.com>
2022-10-09 22:16:29 +0200

    Cleanup

--- src/eventsFromMain/ui.ts
+++ src/eventsFromMain/ui.ts
@@ -3,5 +3,6 @@ import { IpcRendererEvent } from 'electron';
 import { dispatch } from '../store';
 import { setIsLoading } from '../store/entities/ui';
 
-window.api.on('setIsLoading', (e: IpcRendererEvent, isLoading: boolean): any =>
-  dispatch(setIsLoading(isLoading)));
+window.api.on('setIsLoading', (e: IpcRendererEvent, isLoading: boolean) => {
+  dispatch(setIsLoading(isLoading));
+});
