adb514e271f27ef6300427a63c77c4a529568631
Alex Seifert <alex@alexseifert.com>
2022-10-10 15:12:12 +0200

    Only show the loader when isLoading is set

--- src/components/elements/GlobalLoader/index.tsx
+++ src/components/elements/GlobalLoader/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 
+import { useAppSelector } from '../../../store/hooks';
 import Toast from '../Toast';
 
 import styles from './styles.module.scss';
 
 const GlobalLoader: React.FC = () => {
   const { t } = useTranslation(['common']);
+  const { isLoading } = useAppSelector(state => state.ui);
+
+  if (!isLoading) {
+    return null;
+  }
 
   return (
     <Toast hideCloseButton wrapperClassName={styles.loader} className={styles.message}>
