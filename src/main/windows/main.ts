134c1304607429c8cfdfa04b21628a957cfe2c55
Alex Seifert <alex@alexseifert.com>
2022-10-10 12:28:45 +0200

    Saving and restoring window preferences works

--- src/main/windows/main.ts
+++ src/main/windows/main.ts
@@ -1,5 +1,6 @@
 import { BrowserWindow, BrowserWindowConstructorOptions, dialog, Menu, nativeTheme, screen } from 'electron';
 import path from 'path';
+import fs, { promises as fsPromises } from 'fs';
 import log from 'electron-log';
 
 import config from '../../config/main';
@@ -8,21 +9,32 @@ import getAppMenu from '../menus/appMenus/main';
 import { ProjectFile } from '../../shared/interfaces/file';
 import { addToRecentProjects } from '../lib/projectFile';
 import i18n from '../../i18n/main';
+import { WindowPreferences } from '../../interfaces/windows';
 
 const { t } = i18n;
 
+const WINDOW_PREFERENCES_FILE = 'projectWindow.json';
+const WINDOW_PREFERENCES_FILE_PATH = path.resolve(config.storagePath, WINDOW_PREFERENCES_FILE);
+
 export const windows = new Set();
 
-export default async (projectFile?: ProjectFile, filePath?: string): Promise<BrowserWindow> => {
-  // const preferences = load window preferences here;
+const getWindowPreferences = async (): Promise<WindowPreferences> => {
+  const preferencesString = fs.existsSync(WINDOW_PREFERENCES_FILE_PATH)
+    ? await fsPromises.readFile(WINDOW_PREFERENCES_FILE_PATH, 'utf8')
+    : '';
 
+  return preferencesString ? JSON.parse(preferencesString) : {};
+};
+
+export default async (projectFile?: ProjectFile, filePath?: string): Promise<BrowserWindow> => {
+  const preferences = await getWindowPreferences();
   const { width: screenWidth, height: screenHeight } = screen.getPrimaryDisplay().workAreaSize;
   const defaultWidth = Math.floor(screenWidth * 0.6);
   const defaultHeight = Math.floor(screenHeight * 0.75);
 
   const browserWindowOptions: BrowserWindowConstructorOptions = {
-    width: defaultWidth, // preferences.windowWidth || defaultWidth,
-    height: defaultHeight, // preferences.windowHeight || defaultHeight,
+    width: preferences.width || defaultWidth,
+    height: preferences.height || defaultHeight,
     icon: path.join(__dirname, '../../assets/images/icon128.png'),
     webPreferences: {
       contextIsolation: true,
@@ -38,19 +50,19 @@ export default async (projectFile?: ProjectFile, filePath?: string): Promise<Bro
     browserWindowOptions.trafficLightPosition = { x: 10, y: 12 };
   }
 
-  // if (preferences.windowX && preferences.windowY) {
-  //   browserWindowOptions.x = preferences.windowX;
-  //   browserWindowOptions.y = preferences.windowY;
-  // }
+  if (preferences.x && preferences.y) {
+    browserWindowOptions.x = preferences.x;
+    browserWindowOptions.y = preferences.y;
+  }
 
   const newWindow = new BrowserWindow(browserWindowOptions);
 
   if (newWindow) {
-    // if (preferences.windowIsMaximized) {
-    //   newWindow.maximize();
-    // }
+    if (preferences.isMaximized) {
+      newWindow.maximize();
+    }
 
-    // newWindow.setFullScreen(preferences.windowIsFullScreen || false);
+    newWindow.setFullScreen(preferences.isFullScreen || false);
 
     newWindow.loadURL(
       process.env.NODE_ENV === 'development'
@@ -59,25 +71,25 @@ export default async (projectFile?: ProjectFile, filePath?: string): Promise<Bro
     );
 
     newWindow.on('resized', (e: Event) => onWindowChanged(e, {
-      windowWidth: newWindow.getBounds().width,
-      windowHeight: newWindow.getBounds().height,
-      windowIsMaximized: false,
+      width: newWindow.getBounds().width,
+      height: newWindow.getBounds().height,
+      isMaximized: false,
     }));
     newWindow.on('moved', (e: Event) => onWindowChanged(e, {
-      windowX: newWindow.getBounds().x,
-      windowY: newWindow.getBounds().y,
+      x: newWindow.getBounds().x,
+      y: newWindow.getBounds().y,
     }));
     newWindow.on('maximize', (e: Event) => onWindowChanged(e, {
-      windowIsMaximized: newWindow.isMaximized(),
+      isMaximized: newWindow.isMaximized(),
     }));
     newWindow.on('unmaximize', (e: Event) => onWindowChanged(e, {
-      windowIsMaximized: newWindow.isMaximized(),
+      isMaximized: newWindow.isMaximized(),
     }));
     newWindow.on('enter-full-screen', (e: Event) => onWindowChanged(e, {
-      windowIsFullScreen: newWindow.isFullScreen(),
+      isFullScreen: newWindow.isFullScreen(),
     }));
     newWindow.on('leave-full-screen', (e: Event) => onWindowChanged(e, {
-      windowIsFullScreen: newWindow.isFullScreen(),
+      isFullScreen: newWindow.isFullScreen(),
     }));
 
     newWindow.on('close', (e: Event) => onClose(e, newWindow));
@@ -104,39 +116,14 @@ export default async (projectFile?: ProjectFile, filePath?: string): Promise<Bro
   return newWindow;
 };
 
-interface OnWindowChangedOptions {
-  windowWidth?: number;
-  windowHeight?: number;
-  windowX?: number;
-  windowY?: number;
-  windowIsMaximized?: boolean;
-  windowIsFullScreen?: boolean;
-}
-
-const onWindowChanged = async (e: Event, changedPreferences: OnWindowChangedOptions): Promise<void> => {
+const onWindowChanged = async (e: Event, changedPreferences: WindowPreferences): Promise<void> => {
   try {
-    // Save the Window preferences here
-    // const windowsRepository = getRepository(Windows);
-    // const preferences = await windowsRepository.findOne({
-    //   where: {
-    //     windowId: WINDOW_ID,
-    //   },
-    // });
+    const preferences = await getWindowPreferences();
 
-    // if (!preferences) {
-    //   await windowsRepository.save({
-    //     windowId: WINDOW_ID,
-    //     ...changedPreferences,
-    //   });
-    // }
-    // else {
-    //   await windowsRepository.update({
-    //     id: preferences.id,
-    //   }, {
-    //     ...preferences,
-    //     ...changedPreferences,
-    //   });
-    // }
+    await fsPromises.writeFile(WINDOW_PREFERENCES_FILE_PATH, JSON.stringify({
+      ...preferences,
+      ...changedPreferences,
+    }), 'utf8');
   }
   catch (error) {
     log.error(error);
@@ -163,17 +150,6 @@ const onClose = async (e: Event, window?: BrowserWindow): Promise<void> => {
           window?.webContents.send('saveProject', true);
         }
       }
-
-      // const windowBounds = window.getBounds();
-
-      // const windowPreferences = {
-      //   windowWidth: windowBounds.width,
-      //   windowHeight: windowBounds.height,
-      //   windowX: windowBounds.x,
-      //   windowY: windowBounds.y,
-      //   windowIsFullScreen: window.isFullScreen(),
-      //   windowIsMaximized: window.isMaximized(),
-      // };
     }
   }
   catch (error) {
