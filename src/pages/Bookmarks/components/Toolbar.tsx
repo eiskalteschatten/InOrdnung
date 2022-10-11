b11a2351e3776bc5f0a5153f6c5eef8a3add6bdc
Alex Seifert <alex@alexseifert.com>
2022-10-11 12:34:33 +0200

    Update the new bookmark button label

--- src/pages/Bookmarks/components/Toolbar.tsx
+++ src/pages/Bookmarks/components/Toolbar.tsx
@@ -4,11 +4,11 @@ import { useTranslation } from 'react-i18next';
 import ToolbarButtons from '../../../components/elements/ToolbarButtons';
 
 const Toolbar: React.FC = () => {
-  const { t } = useTranslation(['common']);
+  const { t } = useTranslation(['bookmarks']);
 
   const toolbar = [
     {
-      label: t('common:new'),
+      label: t('bookmarks:newBookmark'),
       icon: 'bookmark',
       primary: true,
       onClick: () => console.log('new bookmark'),
