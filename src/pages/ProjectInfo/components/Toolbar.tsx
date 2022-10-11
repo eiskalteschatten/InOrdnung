b219b011f1ab74d5de11a1c2084e5a8eea72f548
Alex Seifert <alex@alexseifert.com>
2022-10-11 15:50:18 +0200

    Navigate to the bookmarks page

--- src/pages/ProjectInfo/components/Toolbar.tsx
+++ src/pages/ProjectInfo/components/Toolbar.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
 import ToolbarButtons from '../../../components/elements/ToolbarButtons';
 import { createNewBookmark } from '../../../shared/lib/bookmarks';
 
 const Toolbar: React.FC = () => {
   const { t } = useTranslation(['bookmarks']);
+  const navigate = useNavigate();
 
   const toolbar = [
     {
       label: t('bookmarks:newBookmark'),
       icon: 'bookmark',
       onClick: () => {
-        // TODO: navigate to boomarks page
+        navigate('/bookmarks');
         createNewBookmark();
       },
     },
