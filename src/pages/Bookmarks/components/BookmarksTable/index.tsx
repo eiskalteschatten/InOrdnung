3bdaafcec4f8c582ff6176c71c3e0812fc010179
Alex Seifert <alex@alexseifert.com>
2022-10-11 12:20:16 +0200

    Sorting now works in the headers

--- src/pages/Bookmarks/components/BookmarksTable/index.tsx
+++ src/pages/Bookmarks/components/BookmarksTable/index.tsx
@@ -62,12 +62,25 @@ const BookmarksTable: React.FC = () => {
           <tr key={headerGroup.id}>
             {headerGroup.headers.map(header => (
               <th key={header.id}>
-                {header.isPlaceholder
-                  ? null
-                  : flexRender(
-                    header.column.columnDef.header,
-                    header.getContext()
-                  )}
+                {header.isPlaceholder ? null : (
+                  <div
+                    {...{
+                      className: header.column.getCanSort()
+                        ? 'cursor-pointer select-none'
+                        : '',
+                      onClick: header.column.getToggleSortingHandler(),
+                    }}
+                  >
+                    {flexRender(
+                      header.column.columnDef.header,
+                      header.getContext()
+                    )}
+                    {{
+                      asc: ' 🔼',
+                      desc: ' 🔽',
+                    }[header.column.getIsSorted() as string] ?? null}
+                  </div>
+                )}
               </th>
             ))}
           </tr>
