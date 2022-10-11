34686bd0198db57fa58e67acad9ee2bdd971fef0
Alex Seifert <alex@alexseifert.com>
2022-10-11 16:26:02 +0200

    Add form fields to the edit bookmark component

--- src/pages/Bookmarks/components/EditBookmark/index.tsx
+++ src/pages/Bookmarks/components/EditBookmark/index.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+
+import Input from '../../../../components/elements/Input';
+
+import styles from './styles.module.scss';
 
 const EditBookmark: React.FC = () => {
+  const { t } = useTranslation(['bookmarks']);
+
+  // if (!editing) {
+  //   return null;
+  // }
+
   return (
-    <div>
-      edit bookmark
+    <div className={styles.editBookmark}>
+      <Input
+        label={t('bookmarks:name')}
+        fullWidth
+      />
+
+      <Input
+        label={t('bookmarks:url')}
+        fullWidth
+        placeholder='https://...'
+      />
     </div>
   );
 };
