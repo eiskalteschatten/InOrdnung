0442e40dc2f51d5863988fff4220e064617841ae
Alex Seifert <alex@alexseifert.com>
2022-10-13 18:32:23 +0200

    Delete the newly created bookmark if the editor is closed and nothing has been filled out

--- src/pages/Bookmarks/index.tsx
+++ src/pages/Bookmarks/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { setEditingId } from '../../store/entities/project/bookmarks';
+import { bookmarkSelectors, deleteBookmark, setEditingId } from '../../store/entities/project/bookmarks';
 
 import Toolbar from './components/Toolbar';
 import ProjectLayout from '../../components/layouts/ProjectLayout';
@@ -14,9 +14,20 @@ import EditBookmark from './components/EditBookmark';
 const Bookmarks: React.FC = () => {
   const { t } = useTranslation(['bookmarks']);
   const dispatch = useAppDispatch();
+  const state  = useAppSelector(state => state);
   const { editingId } = useAppSelector(state => state.project.bookmarks);
 
-  const handleSidebarClose = () => dispatch(setEditingId());
+  const handleSidebarClose = () => {
+    if (editingId) {
+      const bookmarkToEdit = bookmarkSelectors.selectById(state, editingId);
+
+      if (bookmarkToEdit && !bookmarkToEdit.name && !bookmarkToEdit.url) {
+        dispatch(deleteBookmark(editingId));
+      }
+    }
+
+    dispatch(setEditingId());
+  };
 
   return (
     <ProjectLayout toolbar={<Toolbar />}>
