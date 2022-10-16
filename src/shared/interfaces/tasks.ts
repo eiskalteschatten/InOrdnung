baa4934719c9ee9bd94b0894fae2f435d38826ab
Alex Seifert <alex@alexseifert.com>
2022-10-16 13:31:37 +0200

    Add the task interfaces

---
+++ src/shared/interfaces/tasks.ts
@@ -0,0 +1,24 @@
+export interface TaskList {
+  id: string;
+  name: string;
+  view: TaskViewType;
+}
+
+export interface Task {
+  id: string;
+  taskListId: string;
+  status: TaskStatus;
+  name: string;
+  description?: string;
+}
+
+export enum TaskViewType {
+  LIST = 'list',
+  KANBAN_BOARD = 'kanbanBoard',
+}
+
+export enum TaskStatus {
+  TODO = 'todo',
+  DOING = 'doing',
+  DONE = 'done',
+}
