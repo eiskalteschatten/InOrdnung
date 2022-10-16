4a520f9aab426cb8189c1560abef7d325a03f597
Alex Seifert <alex@alexseifert.com>
2022-10-16 19:49:31 +0200

    Add the task list page

---
+++ src/pages/Tasks/TaskList.tsx
@@ -0,0 +1,37 @@
+import React, { useMemo } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { useAppSelector } from '../../store/hooks';
+import { taskListSelectors, taskSelectors } from '../../store/entities/project/tasks';
+
+import { TaskList, TaskListViewType } from '../../shared/interfaces/tasks';
+import ProjectLayout from '../../components/layouts/ProjectLayout';
+import Column from '../../components/elements/Column';
+
+import Toolbar from './components/Toolbar';
+import TasksTable from './components/TasksTable';
+
+const TaskListPage: React.FC = () => {
+  const { editingId } = useAppSelector(state => state.project.tasks);
+  const tasks = useAppSelector(taskSelectors.selectAll);
+  const { taskListId } = useParams();
+  const state = useAppSelector(state => state);
+
+  const taskList = useMemo<TaskList | undefined>(() => {
+    return taskListId ? taskListSelectors.selectById(state, taskListId) : undefined;
+  }, [taskListId]);
+
+  return (
+    <ProjectLayout toolbar={<Toolbar />}>
+      <Column flexGrow padding fullWidth={!editingId}>
+        {taskList?.view === TaskListViewType.KANBAN_BOARD ? (
+          <div>KANBAN_BOARD</div>
+        ) : (
+          <TasksTable tasks={tasks} />
+        )}
+      </Column>
+    </ProjectLayout>
+  );
+};
+
+export default TaskListPage;
--- src/router/Tasks.tsx
+++ src/router/Tasks.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Tasks from '../pages/Tasks';
+import TaskList from '../pages/Tasks/TaskList';
 
 const TasksRouter: React.FC = () => {
   return (
     <Routes>
+      <Route path='/list/:taskListId' element={<TaskList />} />
       <Route path='*' element={<Tasks />} />
     </Routes>
   );
