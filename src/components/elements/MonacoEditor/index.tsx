64cead4ed8bf922ddc0db2b5a423ebeffe66b7fd
Alex Seifert <alex@alexseifert.com>
2022-10-22 00:27:18 +0200

    Set additional options

--- src/components/elements/MonacoEditor/index.tsx
+++ src/components/elements/MonacoEditor/index.tsx
@@ -1,14 +1,34 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect, useMemo, useRef } from 'react';
 import Editor, { EditorProps, useMonaco } from '@monaco-editor/react';
+import { editor as editorApi } from 'monaco-editor/esm/vs/editor/editor.api';
 
 import Spinner from '../Spinner';
 import { useAppSelector } from '../../../store/hooks';
 
 import styles from './styles.module.scss';
 
-const MonacoEditor: React.FC<EditorProps> = ({ theme, defaultLanguage, ...leftoverProps }) => {
+interface Props extends EditorProps {
+  minimap?: boolean;
+  readOnly?: boolean;
+  lineNumbers?: editorApi.LineNumbersType;
+}
+
+const MonacoEditor: React.FC<Props> = props => {
+  const {
+    theme,
+    defaultLanguage = 'markdown',
+    minimap = true,
+    readOnly,
+    lineNumbers = 'off',
+    ...leftoverProps
+  } = props;
+
   const { prefersDarkMode } = useAppSelector(state => state.app);
-  const monaco = useMonaco();
+  const editorRef = useRef<editorApi.IStandaloneCodeEditor | null>(null);
+
+  const handleEditorDidMount = (editor: editorApi.IStandaloneCodeEditor) => {
+    editorRef.current = editor;
+  };
 
   const _theme = useMemo(() => {
     if (theme) {
@@ -19,14 +39,27 @@ const MonacoEditor: React.FC<EditorProps> = ({ theme, defaultLanguage, ...leftov
   }, [theme, prefersDarkMode]);
 
   useEffect(() => {
-    // monaco?.editor
-  }, []);
+    if (editorRef.current) {
+      editorRef.current.updateOptions({ theme: prefersDarkMode ? 'vs-dark' : 'light' });
+    }
+  }, [prefersDarkMode]);
+
+  useEffect(() => {
+    if (editorRef.current) {
+      editorRef.current.updateOptions({
+        minimap: { enabled: minimap },
+        readOnly,
+        lineNumbers,
+      });
+    }
+  }, [minimap, readOnly, lineNumbers]);
 
   return (
     <Editor
       theme={_theme}
-      defaultLanguage={defaultLanguage || 'markdown'}
+      defaultLanguage={defaultLanguage}
       loading={<Spinner className={styles.loader} />}
+      onMount={handleEditorDidMount}
       {...leftoverProps}
     />
   );
