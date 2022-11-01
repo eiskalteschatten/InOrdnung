8d6358406f254be90c0a76d4837947cab13174e0
Alex Seifert <alex@alexseifert.com>
2022-11-01 14:10:14 +0100

    Add the label to the new Markdown editor

--- src/components/elements/MarkdownEditor/index.tsx
+++ src/components/elements/MarkdownEditor/index.tsx
@@ -9,11 +9,17 @@ interface Props extends MDEditorProps {
 
 const MarkdownEditor: React.FC<Props> = ({ label, preview, ...leftoverProps }) => {
   return (
-    <ReactMarkdownEditor
-      toolbarBottom={true}
-      preview={preview ? preview : 'edit'}
-      {...leftoverProps}
-    />
+    <div>
+      {label && (
+        <div className={styles.label}>{label}:</div>
+      )}
+
+      <ReactMarkdownEditor
+        toolbarBottom={true}
+        preview={preview ? preview : 'edit'}
+        {...leftoverProps}
+      />
+    </div>
   );
 };
 
