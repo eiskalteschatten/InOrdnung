87372f6e8f6334786581615636a571006e759e3e
Alex Seifert <alex@alexseifert.com>
2023-02-04 10:32:46 +0100

    Update i18next

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -4,11 +4,18 @@ import { initReactI18next } from 'react-i18next';
 import config from '../config';
 import defaulti18nConfig from './config';
 
+declare module 'i18next' {
+  interface CustomTypeOptions {
+    returnNull: false
+  }
+}
+
 i18n
   .use(initReactI18next)
   .init({
     ...defaulti18nConfig,
     lng: navigator.language.split('-')[0] || config.intl.defaultLocale,
+    returnNull: false,
   });
 
 export default i18n;
